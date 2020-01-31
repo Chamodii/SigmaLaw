6af1a943f72443034d379eca96b94a655219d3b6
Chamodii <chamodii.16@cse.mrt.ac.lk>
2020-01-31 10:41:28 +0530

    Initial requirements for the task.

--- test.py
+++ test.py
@@ -1,19 +1,51 @@
-
 import json
 from stanfordcorenlp import StanfordCoreNLP
 
 nlp = StanfordCoreNLP(r'/home/chamodi/stanford-corenlp-full-2018-10-05', quiet=False)
-props = {'annotators': 'coref', 'pipelineLanguage': 'en'}
+
+##kbp extracts(subject, relation, object) triples from sentences.
+props = {'annotators': 'ner, coref, kbp', 'pipelineLanguage': 'en'}
+
+
+def subject(a):
+    return (a + 0.8) / 2
+
+
+def not_subject(a):
+    return (a + 0.1) / 2
+
 
 text = 'Barack Obama was born in Hawaii.  He is the president. Obama was elected in 2008. His wife is Mitchell. She is nice. That is at least what people think. But they are crazy anyways.'
 
 result = json.loads(nlp.annotate(text, properties=props))
 
-cluster_list = list(result['corefs'].values())
+r=result['sentences']
+
+##NERs
+for each in r:
+  q=each.get("tokens")
+  print(q)
+  for i in q:
+    print(i.get("word"), i.get("ner"))
 
 
-print(cluster_list)
+##Coref clusters
+corefs=result['corefs']
+for each in corefs.values():
+  print(each)
 
-# mentions = result['corefs'].items()[0]
-# for mention in mentions:
-#     print(mention)
+
+##For identifying the subject
+for each in r:
+  print(each.get('kbp'))
+
+
+##### Calculate rough values for party_probability
+parties={}
+init_value = 1.0
+for each in corefs.values():
+  parties[each[0].get('text')] = init_value
+
+
+
+
